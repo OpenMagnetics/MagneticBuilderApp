d6f4f32b7a71e6be276f10621f3108b982856da5
AlfVII <Alfonso_VII@hotmail.com>
2025-06-16 13:25:14 +0200

    Added new plots for material

--- src/components/MagneticBuilder/AdvancedCoreSelector/AdvancedCoreSelectorMaterial.vue
+++ src/components/MagneticBuilder/AdvancedCoreSelector/AdvancedCoreSelectorMaterial.vue
@@ -3,6 +3,7 @@ import InitialPermeabilityVersusTemperature from './AdvancedCoreSelectorMaterial
 import InitialPermeabilityVersusFrequency from './AdvancedCoreSelectorMaterial/InitialPermeabilityVersusFrequency.vue'
 import InitialPermeabilityVersusMagneticFieldDcBias from './AdvancedCoreSelectorMaterial/InitialPermeabilityVersusMagneticFieldDcBias.vue'
 import ComplexPermeabilityVersusFrequency from './AdvancedCoreSelectorMaterial/ComplexPermeabilityVersusFrequency.vue'
+import BhCyclePerTemperature from './AdvancedCoreSelectorMaterial/BhCyclePerTemperature.vue'
 import { deepCopy } from '/WebSharedComponents/assets/js/utils.js'
 import Text from '/WebSharedComponents/DataInput/Text.vue'
 import Dimension from '/WebSharedComponents/DataInput/Dimension.vue'
@@ -33,10 +34,35 @@ export default {
         if (typeof(this.core.functionalDescription.material) == "string") {
             this.loadMaterialData();
         }
+        else {
+            this.loadAdvancedMaterialData();
+        }
     },
     mounted () {
     },
     methods: {
+        loadAdvancedMaterialData() {
+            const url = import.meta.env.VITE_API_ENDPOINT + '/read_advanced_core_material_by_name'
+            const data = {
+                "name": this.core.functionalDescription.material.name
+            }
+            this.$axios.post(url, data)
+            .then(response => {
+                console.log(response.data);
+                if (response.data.bhCycle != null) {
+                    this.core.functionalDescription.material.bhCycle = response.data.bhCycle;
+                }
+                if (response.data.volumetricLosses != null) {
+                    this.core.functionalDescription.material.volumetricLosses = response.data.volumetricLosses;
+                }
+                if (response.data.permeability != null && response.data.permeability.amplitude != null) {
+                    this.core.functionalDescription.material.permeability.amplitude = response.data.permeability.amplitude;
+                }
+            })
+            .catch(error => {
+                console.error(error);
+            });
+        },
         loadMaterialData() {
             this.$mkf.ready.then(_ => {
                 const materialJson = this.$mkf.get_material_data(this.core.functionalDescription.material);
@@ -46,11 +72,12 @@ export default {
                 }
                 else {
                     this.core.functionalDescription.material = JSON.parse(materialJson);
+                    this.loadAdvancedMaterialData();
                 }
                 console.log("this.core.functionalDescription.material.permeability.initial.length")
                 console.log(this.core.functionalDescription.material.permeability.initial.length)
             })
-        }
+        },
     }
 }
 </script>
@@ -126,6 +153,11 @@ export default {
                     v-if="core.functionalDescription.material.permeability != null && core.functionalDescription.material.permeability.complex != null"
                     :dataTestLabel="dataTestLabel + '-ComplexPermeabilityVersusFrequency'"
                     :data="core.functionalDescription.material.permeability.complex"
+                />
+                <BhCyclePerTemperature
+                    v-if="core.functionalDescription.material.bhCycle != null"
+                    :dataTestLabel="dataTestLabel + '-BhCyclePerTemperature'"
+                    :data="core.functionalDescription.material.bhCycle"
                 />
             </div>
         </div>
---
+++ src/components/MagneticBuilder/AdvancedCoreSelector/AdvancedCoreSelectorMaterial/RemanenceVersusTemperature.vue
@@ -0,0 +1,133 @@
+<script setup>
+import DimensionReadOnly from '/WebSharedComponents/DataInput/DimensionReadOnly.vue'
+import PropertyTool from '/WebSharedComponents/Common/PropertyTool.vue'
+import Dimension from '/WebSharedComponents/DataInput/Dimension.vue'
+import ElementFromList from '/WebSharedComponents/DataInput/ElementFromList.vue'
+import { deepCopy } from '/WebSharedComponents/assets/js/utils.js'
+import Text from '/WebSharedComponents/DataInput/Text.vue'
+
+</script>
+
+<script>
+
+export default {
+    emits: ["convertToArray"],
+    props: {
+        dataTestLabel: {
+            type: String,
+            default: '',
+        },
+        data: {
+            type: Object,
+            required: true,
+        },
+        enableSimulation: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    data() {
+        const indexes = [];
+        const configuration = {
+                xAxisLabel: 'temperature',
+                yAxisLabel: 'magneticFluxDensity',
+                xAxisReplaceLabel: 'Temp.',
+                yAxisReplaceLabel: ['Remanence'],
+                xAxisMode: 'linear',
+                yAxisMode: 'linear',
+                xAxisUnit: '°C',
+                yAxisUnit: 'T',
+                xAxisAllowNegative: true,
+                yAxisAllowNegative: false,
+                xAxisDefaultValue: 25,
+                yAxisDefaultValue: 1,
+                xAxisNumberDecimals: 1,
+                yAxisNumberDecimals: 3,
+                xAxisMin: -100,
+                yAxisMin: 0.001,
+                xAxisMax: 300,
+                yAxisMax: 2,
+        }
+
+        const localData = [];
+
+        return {
+            indexes,
+            configuration,
+            localData,
+        }
+    },
+    watch: {
+        'data': {
+            handler(newValue, oldValue) {
+                this.assignLocalData();
+            },
+          deep: false
+        },
+    },
+    created () {
+    },
+    mounted () {
+        if (this.data != null) {
+            if (this.data.length != null) {
+                this.assignLocalData();
+            }
+            else {
+                this.$emit("convertToArray")
+            }
+        }
+    },
+    methods: {
+        assignLocalData() {
+            this.$mkf.ready.then(_ => {
+                if (typeof(this.data) == "object") {
+                    this.localData = [];
+                    this.data.forEach((elem) => {
+                        if (elem.temperature == null) {
+                            const aux = this.$mkf.get_defaults();
+                            elem.temperature = aux.get('ambientTemperature');
+                        }
+                        this.localData.push(elem);
+                    })
+                }
+            })
+        },
+        onRemovePoint(seriesIndex, index) {
+            this.data.splice(this.indexes[index], 1);
+            this.assignLocalData();
+        },
+        onAddPoint(seriesIndex, index) {
+            if (this.localData.length > 0) {
+                const newElement = deepCopy(this.data[this.indexes[index]])
+                this.data.splice(this.indexes[index] + 1, 0, newElement)
+            }
+            else {
+                const aux = {};
+                aux[this.configuration.xAxisLabel] = 0;
+                aux[this.configuration.yAxisLabel] = 0;
+                this.data.push(aux);
+            }
+            this.assignLocalData();
+        },
+        onDimensionUpdate(event, seriesIndex, index) {
+            this.data[this.indexes[index]][event.dimension] = event.value;
+        },
+    }
+}
+</script>
+
+<template>
+    <PropertyTool
+        v-if="data != null"
+        :dataTestLabel="dataTestLabel + '-PropertyTool'"
+        :title="'Remanence vs Temperature'"
+        :properties="[localData]"
+        :propertiesConfiguration="configuration"
+        :chartStyle="'height: 20vh'"
+        :chartPaddings="{top: 30, left: 60, right: 10, bottom: 30}"
+        :smoothLine="true"
+        @onRemovePoint="onRemovePoint"
+        @onAddPoint="onAddPoint"
+        @onDimensionUpdate="onDimensionUpdate"
+    />
+</template>
